e2394cd599875b191a69debf66b05ca45decd4d8
jing.chen <jing.chen@getinsured.com>
2018-04-26 14:10:56 -0700

    Update code for movie manager.

--- movie-manager/app.js
+++ movie-manager/app.js
@@ -1,19 +1,2 @@
-require('dotenv').config();
-require('morgan');
-const http = require('http');
-
-const welcome = (req, res) => {
-    console.log(req.url);
-    console.log('Welcome to movie manager.');
-    res.write('Welcome to movie manager.');
-    res.end();
-}
-
-const server = http.createServer(welcome);
-
-const port = process.env.APP_PORT || 3000;
-
-server.listen(port, () => {
-    console.log('server is running on port ' + port);
-});
+const app = require('./express');
 
