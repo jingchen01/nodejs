ba6ff0d0e1f0bccded8a75136b719179841dee52
jing.chen <jing.chen@getinsured.com>
2018-04-26 15:35:52 -0700

    Update app.js

---
+++ movie-manager/api/models/comedyMovieModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const comedyMovieModel = new Schema({
+    _id: Schema.Types.ObjectId,
+    title: String,
+    rating: Number,
+    price: Number,
+    genre: {
+        type: String,
+        default: 'Comedy'
+    },
+    isReleased: {
+        type: Boolean,
+        default: true
+    }
+});
+
+module.exports = mongoose.Schema('ComedyMovie', comedyMovieModel);
